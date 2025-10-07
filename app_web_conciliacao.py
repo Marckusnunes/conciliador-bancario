7f8c29eafd1c2f9d754499035ab2c71579293500
Marckusnunes <marckus.barros@prefeitura.rio>
2025-10-07 09:47:46 -0300

    Update app_web_conciliacao.py

--- app_web_conciliacao.py
+++ app_web_conciliacao.py
@@ -197,7 +197,7 @@ for col in ['Saldo_Corrente_Extrato', 'Saldo_Aplicado_Extrato']:
     if 'Saldo_Aplicado_Extrato' not in df.columns:
     df['Saldo_Aplicado_Extrato'] = 0
 
-    return df
+        return df
 
 def realizar_conciliacao(df_contabil, df_extrato_unificado):
     """
@@ -415,6 +415,7 @@ if 'df_resultado' in st.session_state and st.session_state['df_resultado'] is no
             st.subheader("Auditoria do Extrato da Caixa Econômica (com Chave Primária)")
             if 'audit_cef' in st.session_state and st.session_state['audit_cef'] is not None:
                 st.dataframe(st.session_state['audit_cef'])
+
 
 
 
